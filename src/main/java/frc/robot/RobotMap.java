212d7426cdf73bc12c7542037b30ff3981fcfdbb
Sidd Rao <siddrao.926@gmail.com>
2022-02-22 00:08:47 -0800

    Added Robot Hardware Map

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+public class RobotMap {
+    public static final class DriveBase {
+        public static final int FRONT_LEFT_MODULE_DRIVE = 31;
+        public static final int FRONT_LEFT_MODULE_STEER = 32;
+        public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 30;
+
+        public static final int FRONT_RIGHT_MODULE_DRIVE = 34;
+        public static final int FRONT_RIGHT_MODULE_STEER = 35;
+        public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 33;
+
+        public static final int BACK_LEFT_MODULE_DRIVE = 37;
+        public static final int BACK_LEFT_MODULE_STEER = 38;
+        public static final int BACK_LEFT_MODULE_STEER_ENCODER = 36;
+
+        public static final int BACK_RIGHT_MODULE_DRIVE = 40;
+        public static final int BACK_RIGHT_MODULE_STEER = 41;
+        public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 39;
+
+        public static final int PIGEON_IMU = 45;
+    }
+
+}
